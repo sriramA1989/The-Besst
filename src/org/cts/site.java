f57af2f8b17fa74a87efc2133b45654d8e4c5ab1
sriramA1989 <sriram.jaihind123@gmail.com>
2019-11-25 11:07:59 +0530

    add

---
+++ src/org/cts/site.java
@@ -0,0 +1,13 @@
+package org.cts;
+
+public class site {
+
+	public void vendor() {
+		System.out.println("site ID");
+
+	}
+	public static void main(String[] args) {
+		site j = new site();
+		j.vendor();
+	}
+}
